711bfa3419201c61bb4890d390562c8ebf1552d5
sakshi <17june2001@gmail.com>
2021-09-06 13:31:26 +0530

    added all files

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-  
 <?php
-	$host= "ddpractice_db_1";
-	$user= "root";
-	$pass= "root";
-	$db="test_db";
+        $host = 'db';
 
-	$conn= new mysqli($host, $user,$pass, $db);
+        $user = 'test';
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	echo($conn->server_version());
-?>
+        $pass = 'test';
+
+        $conn = new mysqli($host, $user, $pass);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            echo "Connected to MySQL server successfully!";
+        }
+
+        echo "<br>your MYSQL client version is -> ";
+        echo mysqli_get_client_version();
+        echo "<br>your MYSQL server version is -> ";
+        echo $conn -> server_version;
+?>  
